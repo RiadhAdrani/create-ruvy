e58d8045b07ec3306a75b0694e45e4ebf511b5f1
RiadhAdrani <riadh_adrani@hotmail.fr>
2024-04-06 12:14:20 +0100

    fix(ruvy-ts): app template

--- templates/ruvy-ts/src/main.tsx
+++ templates/ruvy-ts/src/main.tsx
@@ -1,7 +1,7 @@
 import './style.css';
-import { mountApp, useState } from '@riadh-adrani/ruvy';
+import { ErrorBoundary, mountApp, useState } from '@riadh-adrani/ruvy';
 
-const hostElement = document.querySelector('#app') as HTMLElement;
+const host = document.querySelector('#app') as HTMLElement;
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -9,27 +9,29 @@ const App = () => {
   const onClick = () => setCount(count + 1);
 
   return (
-    <div>
-      <div style={{ display: 'flex' }}>
-        <img src={'./vite.svg'} class={'logo vite-logo'} alt="Vite logo" />
-        <img src={'./ruvy.svg'} class={'logo ruvy-logo'} alt="Ruvy logo" />
+    <ErrorBoundary fallback={'Something went wrong'}>
+      <div>
+        <div style={{ display: 'flex' }}>
+          <img src={'./vite.svg'} class={'logo vite-logo'} alt="Vite logo" />
+          <img src={'./ruvy.svg'} class={'logo ruvy-logo'} alt="Ruvy logo" />
+        </div>
+        <h1>Vite + Ruvy</h1>
+        <p style={{ marginTop: '-20px' }}>
+          <sub>Ruvy is a front-end framework inspired from React.</sub>
+          <br />
+          <sub>It is built for showcase purposes only.</sub>
+        </p>
+        <button onClick={onClick}>
+          You clicked : {count} time{count > 1 ? 's' : ''}
+        </button>
+        <p>
+          <a href="https://github.com/RiadhAdrani/ruvy" target="_blank">
+            <span>Ruvy on GitHub</span>
+          </a>
+        </p>
       </div>
-      <h1>Vite + Ruvy</h1>
-      <p style={{ marginTop: '-20px' }}>
-        <sub>Ruvy is a front-end framework inspired from React.</sub>
-        <br />
-        <sub>It is built for showcase purposes only.</sub>
-      </p>
-      <button onClick={onClick}>
-        You clicked : {count} time{count > 1 ? 's' : ''}
-      </button>
-      <p>
-        <a href="https://github.com/RiadhAdrani/ruvy" target="_blank">
-          <span>Ruvy on GitHub</span>
-        </a>
-      </p>
-    </div>
+    </ErrorBoundary>
   );
 };
 
-mountApp({ hostElement, callback: App });
+mountApp({ host, app: <App /> });
